be27844b1ff2ad1b6e3bce26adba4264e53fb710
offbynull <offbynull@gmail.com>
2015-12-27 16:38:36 -0800

    Added support for IPv6 text parsing.

--- src/main/java/com/offbynull/portmapper/common/TextUtils.java
+++ src/main/java/com/offbynull/portmapper/common/TextUtils.java
@@ -32,7 +32,9 @@ public final class TextUtils {
     // we want to keep the code as light as possible.
     
     private static final int IPV4_COMPONENT_MAX = 255;
+    private static final int IPV4_COMPONENT_MAX_SIZE = 3;
     private static final int IPV6_MAX_NUMBER_OF_COMPONENTS = 8;
+    private static final int IPV6_COMPONENT_MAX_SIZE = 4;
 
     private TextUtils() {
         // do nothing
@@ -111,6 +113,10 @@ public final class TextUtils {
             }
 
             String componentStr = component.toString();
+            
+            // make sure we only read a max of 3 bytes
+            Validate.isTrue(componentStr.length() <= IPV4_COMPONENT_MAX_SIZE);
+            
             int componentAsNum = Integer.parseInt(componentStr);
 
             // not a IP4 component if it has has trailing zeros
@@ -145,7 +151,7 @@ public final class TextUtils {
         for (int i = 0; i < len;) {
             int cp = text.codePointAt(i);
             try {
-                String ip = readIp4(text, i);
+                String ip = readIp6(text, i);
                 i += ip.length();
                 
                 ret.add(ip);
@@ -173,15 +179,63 @@ public final class TextUtils {
                 offset++;
             }
             
-            // If less than 8 components, must have exactly 1 empty component -- empty = group of zeroes
             if (components.size() < IPV6_MAX_NUMBER_OF_COMPONENTS) {
-                int emptyComponentCount = 0;
-                for (String component : components) {
-                    if (component.isEmpty()) {
-                        emptyComponentCount++;
-                    }
+                // If less than 8 components, must have 1 empty group ...
+                // "::" is a valid address  (has 3 empty components at start -- note that this means 1 empty group)
+                // "::1" is a valid address  (has 2 empty components at start -- note that this means 1 empty group)
+                // "1::" is a valid address  (has 2 empty components at end -- note that this means 1 empty group)
+                // "1::1" is a valid address  (has 1 empty component in the middle -- note that this means 1 empty group)
+                // "0:0::0" is a valid address  (has 1 empty component in the middle -- note that this means 1 empty group)
+                // "0::0::0" is NOT a valid address  (has 2 empty component in the middle -- note that this means 2 empty group)
+        
+                if (components.size() == 3
+                        && components.get(0).isEmpty()
+                        && components.get(1).isEmpty()
+                        && components.get(2).isEmpty()) {
+                    // SPECIAL CASE:
+                    // If 3 empty components, then this is a valid address that means full 0...
+                    // "::" is a valid address        
+                    
+                    // do nothing -- all 3 components are empty so this is valid
+                } else if (components.size() > 2
+                        && components.get(0).isEmpty()
+                        && components.get(1).isEmpty()) {
+                    // SPECIAL CASE:
+                    // If more than 2 components, but first 2 componetns are empty, then this is a valid address that starts with 0s...
+                    // "::1:2" is a valid address        
+                    
+                    Validate.isTrue(2 == getEmptyComponentCount(components)); // already made sure there's 2 in the start, making sure there
+                                                                              // are no others by doing this check
+                } else if (components.size() > 2
+                        && components.get(components.size() - 2).isEmpty()
+                        && components.get(components.size() - 1).isEmpty()) {
+                    // SPECIAL CASE:
+                    // If more than 2 components, but last 2 componetns are empty, then this is a valid address that ends with 0s...
+                    // "1:2::" is a valid address        
+                    
+                    Validate.isTrue(2 == getEmptyComponentCount(components)); // already made sure there's 2 in the end, making sure there
+                                                                              // are no others by doing this check.
+                } else if (components.size() > 2
+                        && !components.get(0).isEmpty()
+                        && !components.get(components.size() - 1).isEmpty()) {
+                    // SPECIAL CASE:
+                    // If more than 2 components, but the first and last components are not empty, there must be at least 1 empty component
+                    // in the middle...
+                    // "1::2" is a valid address        
+                    Validate.isTrue(1 == getEmptyComponentCount(components));
+                } else {
+                    // This is a bad address... addresses that don't have enough components + property empty components end up here ..
+                    // "" is an invalid address
+                    // "abcd" is an invalid address
+                    // "abcd:" is an invalid address
+                    // ":abcd:" is an invalid address
+                    // "abcd:abcd:" is an invalid address
+                    throw new IllegalArgumentException();
                 }
-                Validate.isTrue(emptyComponentCount == 1);
+            } else {
+                // Otherwise must have exactly 8 non-empty components
+                Validate.isTrue(components.size() == IPV6_MAX_NUMBER_OF_COMPONENTS);
+                Validate.isTrue(0 == getEmptyComponentCount(components));
             }
 
             // Construct IP
@@ -190,7 +244,7 @@ public final class TextUtils {
                 String component = components.get(i);
                 ip.append(component);
                 
-                if (i != components.size()) {
+                if (i != components.size() - 1) {
                     ip.append(':');
                 }
             }
@@ -199,6 +253,16 @@ public final class TextUtils {
         } catch (IndexOutOfBoundsException e) {
             throw new IllegalArgumentException(e);
         }
+    }
+
+    private static int getEmptyComponentCount(List<String> components) {
+        int emptyComponentCount = 0;
+        for (String component : components) {
+            if (component.isEmpty()) {
+                emptyComponentCount++;
+            }
+        }
+        return emptyComponentCount;
     }
 
     private static String readIp6Component(String text, int offset) {
@@ -218,11 +282,16 @@ public final class TextUtils {
                 offset += Character.charCount(cp);
             }
 
+            String componentStr = component.toString();
+            
+            // make sure we only read a max of 4 bytes
+            Validate.isTrue(componentStr.length() <= IPV6_COMPONENT_MAX_SIZE);
+
             // no need to check for leading 0's -- leading zeros may be omitted
             
             // may be empty -- empty means 0000
             
-            return component.toString();
+            return componentStr;
         } catch (NumberFormatException | IndexOutOfBoundsException e) {
             throw new IllegalArgumentException(e);
         }
--- src/test/java/com/offbynull/portmapper/common/TextUtilsTest.java
+++ src/test/java/com/offbynull/portmapper/common/TextUtilsTest.java
@@ -98,4 +98,117 @@ public class TextUtilsTest {
         assertEquals(1, addresses.size());
         assertEquals("10.6.0.2", addresses.get(0));
     }
+    
+    @Test
+    public void mustPassWithFullIpv6Components() {
+        List<String> addresses = TextUtils.findAllIpv6Addresses(
+                "sf sdf sd 0123:1234:2345:3456:4567:5678:6789:789a fs\n\n \r\tfsf89ab:9abc:abcd:bcde:cdef:def0:ef01:f012sdfsfsdfs");
+        
+        assertEquals(2, addresses.size());
+        assertEquals("0123:1234:2345:3456:4567:5678:6789:789a", addresses.get(0));
+        assertEquals("89ab:9abc:abcd:bcde:cdef:def0:ef01:f012", addresses.get(1));
+    }
+
+    @Test
+    public void mustPassWithNonFullIpv6Components() {
+        List<String> addresses = TextUtils.findAllIpv6Addresses(
+                "sf sdf sd 0:1:2:3:4:5:6:7 fs\n\n \r\tfsf8:9:a:b:c:d:e:fsdfsfsdfs");
+        
+        assertEquals(2, addresses.size());
+        assertEquals("0:1:2:3:4:5:6:7", addresses.get(0));
+        assertEquals("f8:9:a:b:c:d:e:f", addresses.get(1));
+    }
+
+    @Test
+    public void mustPassWithEmptyIpv6() {
+        List<String> addresses = TextUtils.findAllIpv6Addresses(
+                "sf sdf sd :: fs\n\n \r\tfsf8:9:a:b:c:d:e:fsdfsfsdfs");
+        
+        assertEquals(2, addresses.size());
+        assertEquals("::", addresses.get(0));
+        assertEquals("f8:9:a:b:c:d:e:f", addresses.get(1));
+    }
+
+    @Test
+    public void mustPassWithIpv6ComponentsThatHaveStartingEmptyGroup() {
+        List<String> addresses = TextUtils.findAllIpv6Addresses(
+                "sf sdf sd ::a:b:c fs\n\n \r\tfsf8:9:a:b:c:d:e:fsdfsfsdfs");
+        
+        assertEquals(2, addresses.size());
+        assertEquals("::a:b:c", addresses.get(0));
+        assertEquals("f8:9:a:b:c:d:e:f", addresses.get(1));
+    }
+
+    @Test
+    public void mustPassWithIpv6ComponentsThatHaveEndingEmptyGroup() {
+        List<String> addresses = TextUtils.findAllIpv6Addresses(
+                "sf sdf sd a:b:c:: fs\n\n \r\tfsf8:9:a:b:c:d:e:fsdfsfsdfs");
+        
+        assertEquals(2, addresses.size());
+        assertEquals("a:b:c::", addresses.get(0));
+        assertEquals("f8:9:a:b:c:d:e:f", addresses.get(1));
+    }
+
+    @Test
+    public void mustPassWithIpv6ComponentsThatHaveMiddleEmptyGroup() {
+        List<String> addresses = TextUtils.findAllIpv6Addresses(
+                "sf sdf sd a::c fs\n\n \r\tfsf8:9:a:b:c:d:e:fsdfsfsdfs");
+        
+        assertEquals(2, addresses.size());
+        assertEquals("a::c", addresses.get(0));
+        assertEquals("f8:9:a:b:c:d:e:f", addresses.get(1));
+    }
+
+    @Test
+    public void mustRejectWithIpv6ComponentsThatHaveMoreThan1EmptyGroup() {
+        List<String> addresses = TextUtils.findAllIpv6Addresses(
+                "sf sdf sd ::a::c a::c:: a::b::c fs\n\n \r\tfsf8:9:a:b:c:d:e:fsdfsfsdfs");
+        
+        // Remember that this is correct ... it reads until it determines that what its read is invalid/valid. If it ends up being invalid,
+        // it moves up 1 character from where it originally started reading from and restarts the process.
+        assertEquals(4, addresses.size());
+        assertEquals("a::c", addresses.get(0)); // e.g. ::a::c is valid, so is :a::c, which leads to a::c
+        assertEquals("c::", addresses.get(1));
+        assertEquals("b::c", addresses.get(2));
+        assertEquals("f8:9:a:b:c:d:e:f", addresses.get(3));
+    }
+
+    @Test
+    public void mustRejectWithIpv6WithTooManyComponents() {
+        List<String> addresses = TextUtils.findAllIpv6Addresses(
+                "sf sdf sd 0:1:2:3:4:5:6:7:8:9 fs\n\n \r\tfsf8:9:a:b:c:d:e:fsdfsfsdfs");
+        
+        // Remember that this is correct ... it reads until it determines that what its read is invalid/valid. If it ends up being invalid,
+        // it moves up 1 character from where it originally started reading from and restarts the process.
+        assertEquals(2, addresses.size());
+        assertEquals("0:1:2:3:4:5:6:7", addresses.get(0));
+        assertEquals("f8:9:a:b:c:d:e:f", addresses.get(1));
+    }
+
+    @Test
+    public void mustRejectWithIpv6WithNotEnoughComponents() {
+        List<String> addresses = TextUtils.findAllIpv6Addresses(
+                "sf sdf sd 0:1:2:3:4:5:6 fs\n\n \r\tfsf8:9:a:b:c:d:e:fsdfsfsdfs");
+
+        assertEquals(1, addresses.size());
+        assertEquals("f8:9:a:b:c:d:e:f", addresses.get(0));
+    }
+
+    @Test
+    public void mustRejectWithIpv6WithNotEnoughComponentsWithTrailingColon() {
+        List<String> addresses = TextUtils.findAllIpv6Addresses(
+                "sf sdf sd 0:1:2:3:4:5:6: fs\n\n \r\tfsf8:9:a:b:c:d:e:fsdfsfsdfs");
+
+        assertEquals(1, addresses.size());
+        assertEquals("f8:9:a:b:c:d:e:f", addresses.get(0));
+    }
+    
+    @Test
+    public void mustRejectWithIpv6WithNotEnoughComponentsWithStartingColon() {
+        List<String> addresses = TextUtils.findAllIpv6Addresses(
+                "sf sdf sd :0:1:2:3:4:5:6 fs\n\n \r\tfsf8:9:a:b:c:d:e:fsdfsfsdfs");
+
+        assertEquals(1, addresses.size());
+        assertEquals("f8:9:a:b:c:d:e:f", addresses.get(0));
+    }
 }
